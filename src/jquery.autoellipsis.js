23b7f9ee73cfa4d4cfa96663af5f76ff37daf793
Henri De Veene <henri.deveene@gmail.com>
2013-09-18 10:45:35 +0200

    Throws exception when text contains <BR>. Fix by andrewkolesnikov

--- src/jquery.autoellipsis.js
+++ src/jquery.autoellipsis.js
@@ -267,7 +267,7 @@
                 lastTextNode.get(0).nodeValue = text;
 
             } else {
-                lastTextNode.get(0).nodeValue = '';
+                lastTextNode.remove();
             }
 
             return true;
